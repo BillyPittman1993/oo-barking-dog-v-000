0c46a59048231fccdd9f8d31cb418a732f066946
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-27 19:23:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,25 @@
 # Your code goes here!
+class Dog 
+  
+  def name=(dog_name, bark)
+    @dog_name = name 
+    @bark = bark 
+  end
+  
+  def name 
+    @name 
+  end
+  
+  def bark 
+    @bark 
+  end 
+  
+end
 
+fido = Dog.new
+fido.name = "Fido"
+ 
+puts fido.name
 
-
-
-
-
+ 
+puts fido.bark
